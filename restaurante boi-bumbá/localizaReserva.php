bbe430620907417064c46dfc7dd60a5dd106bfdd
jxavierjorge <44382545+jxavierjorge@users.noreply.github.com>
2018-11-02 22:03:01 -0300

    Add files via upload

--- restaurante boi-bumbá/localizaReserva.php
+++ restaurante boi-bumbá/localizaReserva.php
@@ -1,15 +1,24 @@
   <?php require_once("conecta.php") ?>
 
 <html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
   <body>
-	<fieldset style="width:25%;">
-		<legend>Buscar Reserva</legend>
+  	<center>
+	<fieldset id="pesquisa">
+		<legend> &nbsp Buscar Reserva &nbsp </legend>
 		<form method="POST" action="recebeLocalizaReserva.php?a=buscar">
 			<label>Nome: </label>
 			<input type="text" name="nome" />
-			<input type ="submit" value="Buscar" />
+			<br>
+			<br>
+			<button type="submit">Buscar</button>
+			<hr>
+			<p>Obs: se quiser conferir todas as reservas registradas, favor deixar o campo vazio</p>
 		</form>
 	</fieldset>
+	</center>
  </body>
 </html>
 
