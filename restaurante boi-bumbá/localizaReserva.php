4bdb4b4fe60e6afdd5745b801e0543994cefe20b
jxavierjorge <44382545+jxavierjorge@users.noreply.github.com>
2018-11-02 21:56:14 -0300

    Formatação

--- restaurante boi-bumbá/localizaReserva.php
+++ restaurante boi-bumbá/localizaReserva.php
@@ -1,15 +1,22 @@
   <?php require_once("conecta.php") ?>
 
 <html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
   <body>
-	<fieldset style="width:25%;">
-		<legend>Buscar Reserva</legend>
+  	<center>
+	<fieldset id="pesquisa">
+		<legend> &nbsp Buscar Reserva &nbsp </legend>
 		<form method="POST" action="recebeLocalizaReserva.php?a=buscar">
 			<label>Nome: </label>
 			<input type="text" name="nome" />
-			<input type ="submit" value="Buscar" />
+			<br>
+			<br>
+			<button type="submit">Buscar</button>
 		</form>
 	</fieldset>
+	</center>
  </body>
 </html>
 
